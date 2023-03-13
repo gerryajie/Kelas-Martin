36558e3aa82097a2ed6c4f3f7badcf5b539a99ed
gerry <gerryajie@gmail.com>
2023-03-14 00:10:05 +0700

    martin

---
+++ day3/switch.js
@@ -0,0 +1,15 @@
+let nomor =2
+    switch (nomor){
+        case 1:
+            console.log('Pria');
+            break;
+        case 2:
+            console.log('wanita')
+            break;
+        case 3:
+            console.log('waria');
+            break;
+        default:
+            console.log('kosong')
+    }
+   
---
+++ day3/while.js
@@ -0,0 +1,15 @@
+// let a=0;
+
+// while(a<100){
+//     console.log(`${a} Aku martin`)
+//     a++
+// }
+
+
+let kelasMartin=["martin","gerry","rosa","jokowi","prabowo","lena"]
+
+let index=0;
+while(index<kelasMartin.length){
+    console.log(`${kelasMartin[index]} adalah mahasiswa`)
+    index++
+}
